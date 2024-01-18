8745f38f58115f0b4162854c579f174620460e43
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-01-18 16:40:31 +0900

    complete youtube section
    youtube section 구현 완료

--- src/components/section/YoutubeSection.tsx
+++ src/components/section/YoutubeSection.tsx
@@ -1,11 +1,13 @@
+import YouTube from "react-youtube";
 import floating1 from "../../images/floating1.png";
 import floating2 from "../../images/floating2.png";
+import { Autoplay } from "swiper/modules";
 
 const YoutbueSection = () => {
   return (
     <section className="youtube">
       <div className="youtube__area">
-        <div></div>
+        <YoutubeIFrame videoId="An6LvWQuj_8" classList={["player"]}/>
       </div>
       <div className="youtube__cover">
         <div className="inner">
@@ -16,6 +18,33 @@ const YoutbueSection = () => {
     </section>
   );
 };
+
+const YoutubeIFrame = (props: YoutubeIFrameProps) => {
+
+  const handleReady = (event: any) => {
+    event.target.playVideo();
+    event.target.mute();
+  } 
+
+  return (
+    <YouTube className={props.classList?.join(" ")??""} videoId={props.videoId} onReady={ handleReady } opts={{
+      width: "100%",
+      height: "100%",
+      playerVars: {
+        autoplay: 1, // true 대신에 1을 사용
+        loop: 1, // true 대신에 1을 사용
+        rel: 0, // false 대신에 0을 사용
+        modestbranding: 1, // true 대신에 1을 사용
+        playlist: props.videoId,
+      },
+    }}></YouTube>
+  );
+};
+
+interface YoutubeIFrameProps {
+  videoId: string,
+  classList?: string[]
+}
 
 export default YoutbueSection;
 
