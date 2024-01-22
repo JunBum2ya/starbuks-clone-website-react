70eb92a6f4127db89a0758f92ebbe10bc94de82e
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-01-22 10:19:17 +0900

    complete find store section
    find store section 구현 완료

---
+++ src/components/section/FindStoreSection.tsx
@@ -0,0 +1,29 @@
+import texture1 from "../../images/find_store_texture1.png";
+import texture2 from "../../images/find_store_texture2.png";
+import picture1 from "../../images/find_store_picture1.jpg";
+import picture2 from "../../images/find_store_picture2.jpg";
+import title from "../../images/find_store_text1.png";
+import description from "../../images/find_store_text2.png";
+import CustomButton from "../CustomButton";
+
+const FindStoreSection = () => {
+  return (
+    <section className="find-store scroll-spy">
+      <div className="inner">
+        <img className="texture1" src={texture1} alt="TEXTURE"/>
+        <img className="texture2" src={texture2} alt="TEXTURE"/>
+        <img className="picture picture1 back-to-position to-right delay-0" src={picture1} alt="PICTURE"/>
+        <img className="picture picture2 back-to-position to-right delay-1" src={picture2} alt="PICTURE"/>
+        <div className="text-group">
+          <img className="title back-to-position to-left delay-0" src={title} alt="TITLE"/>
+          <img className="description back-to-position to-left delay-1" src={description} alt="DESCRIPTION"/>
+          <div className="more back-to-position to-left delay-2">
+            <CustomButton>매장찾기</CustomButton>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FindStoreSection;
